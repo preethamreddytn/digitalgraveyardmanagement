12c4e938184bee5eb1a214fc6d7fe5f647c691fa
Preetham Reddy T N <preethamreddytn@gmail.com>
2025-06-12 15:44:14 +0530

    after relocating static files

--- graveweb/settings.py
+++ graveweb/settings.py
@@ -130,10 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR/'web'/'static'],
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web', 'static')]
 
-
-STATIC_ROOT = BASE_DIR, 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
