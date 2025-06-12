4353dca8e36f4a479d15eb6751eeb08bb6d8fd6a
Preetham Reddy T N <preethamreddytn@gmail.com>
2025-06-12 21:43:59 +0530

    after

--- graveweb/settings.py
+++ graveweb/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'web','static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
