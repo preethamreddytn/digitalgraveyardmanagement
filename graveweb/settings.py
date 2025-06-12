9c648e11165fb0b5987c2b1119e70911dad05594
Preetham Reddy T N <preethamreddytn@gmail.com>
2025-06-12 16:02:23 +0530

    Fix: Added STATICFILES_STORAGE for Whitenoise static serving

--- graveweb/settings.py
+++ graveweb/settings.py
@@ -133,6 +133,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web', 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
